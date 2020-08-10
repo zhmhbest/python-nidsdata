d3cdd5962405df4b9c5afc528553b49b5cecd589
zhmhbest <zhmhbest@gmail.com>
2020-08-10 19:07:13 +0800

    依赖修正

--- build.py
+++ build.py
@@ -5,6 +5,6 @@ import os
 if __name__ == '__main__':
     os.system('rmdir /s /q "build"2>nul')
     os.system('rmdir /s /q "dist"2>nul')
-    os.system('rmdir /s /q "python_zhmh.egg-info"2>nul')
+    os.system('rmdir /s /q "nidsdata.egg-info"2>nul')
     os.system('python setup.py sdist bdist_wheel')
-    # os.system("twine upload dist/*")
+    # twine upload dist/*
--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', 'r', encoding='utf-8') as fp:
 
 setuptools.setup(
     python_requires='>=3',
-    install_requires=['pandas>=0.24.2', 'numpy>=1.14.0', 'python-home>=0.0.8'],
+    install_requires=['pandas>=0.24.2', 'numpy>=1.14.0', 'python-zhmh>=0.0.8'],
     packages=setuptools.find_packages(),
     include_package_data=True,
     author="zhmh",
     author_email="zhmhbest@gmail.com",
     name="nidsdata",
-    version='0.0.1',
+    version='0.0.2',
     description="Python Common Command",
     keywords="nids data",
     license="MIT",  # https://choosealicense.com/
